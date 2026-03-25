9ae868133dedbcd3c4c9ef45c618340f94646c34
csanti1092-hash <csanti1092@gmail.com>
2026-03-25 13:28:15 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,1 @@
-# src/__init__.py
-# Archivo vacío - solo marca el directorio como paquete Python
-# Opcional: exponer la función principal
-from .app import lambda_handler
-__all__ = ["lambda_handler"]
+#ddd
