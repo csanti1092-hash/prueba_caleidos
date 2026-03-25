e0c9cc3090c80525c35fb3d9746fffdf73470bf5
csanti1092-hash <csanti1092@gmail.com>
2026-03-25 17:45:06 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,6 +4,6 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Hello World"}),
+        "body": json.dumps({"message": "Hola Mundo"}),
     }
 
